259c23f1d3771d13d6562fee91946bc38909325c
Morteza Kazemi <azetrom.kazemi2000@gmail.com>
2022-08-03 18:42:59 +0430

    create structure for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-navbar">
+            visual qustion answering
+        </div>
+
+        <header className="App-body">
+          <div className="upload-section">
+              <input type="file"/>
+          </div>
+          <img className="image-section" alt="the image you uploaded">
+
+          </img>
+          <div className="question-section">
+              ask a question
+          </div>
+          <div className="answer-section">
+              this is the answer
+          </div>
+            <p>hello hey</p>
+        </header>
     </div>
   );
 }
