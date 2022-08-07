98bf4287cb8756cf4b78cee8d629b28c328d2b39
Morteza Kazemi <azetrom.kazemi2000@gmail.com>
2022-08-07 11:20:39 +0430

    implement basic stable server that provides inputs for the python script

--- server/index.js
+++ server/index.js
@@ -1,44 +1,41 @@
-var express = require('express');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer')
 const path = require("path");
 const {spawn} = require('child_process');
+const fs = require('fs');
 
-var app = express();
-var port = process.env.PORT || 4000;
+const app = express();
+const port = process.env.PORT || 4000;
 
 
 app.use(cors()); // enable CORS
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
-// app.use(bodyParser.raw());
-app.use('/uploads', express.static('uploads'));// serving static files
-
-// request handlers
-app.get('/', (req, res) => {
-    res.send('Node js file upload rest apis');
-});
+// app.use('/uploads', express.static('uploads'));// serving static files //todo should I use this?!
 
 // handle storage using multer
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads');
+        cb(null, '../vqa/input');
     },
     filename: function (req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+        // cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+        cb(null, `${file.fieldname}${path.extname(file.originalname)}`);
     }
 });
 
-// var upload = multer({ storage: storage }); //todo
-var upload = multer(); //todo
+//todo delete the image and question when you are done! image extension may differ -> overwriting it won't work!
+
+const upload = multer({storage: storage});
 
 
 // handle single file upload
 app.post('/upload-file',upload.single('image'),(req, res, next) => {
 
     const image = req.file; //todo this is experimental and might not have a good browser compatibility
-    const question = req.body;
+    const question = req.body.question;
 
     if (!image) {
         return res.status(400).send({message: 'Please upload an image.'});
@@ -46,7 +43,15 @@ app.post('/upload-file',upload.single('image'),(req, res, next) => {
     if (!question){
         return res.status(400).send({ message: 'Please upload a question.' });
     }
-    //todo run the python code here and send the result!
+
+    fs.writeFileSync("../vqa/input/question.txt", question, function(err) { //synchronous file write! (program execution waits...)
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Question file was saved!");
+    });
+
+    //run the python code and send the result!
     let answer;
     const python = spawn('python', ['../vqa/script1.py']); // spawn new child process to call the python script
     // collect data from script
@@ -57,12 +62,9 @@ app.post('/upload-file',upload.single('image'),(req, res, next) => {
     // in close event we are sure that stream from child process is closed
     python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        // res.send(dataToSend)
         console.log(answer)
         return res.send({message: 'upload successful.', question, answer, image});
     });
-
 });
 
 app.listen(port, () => {
